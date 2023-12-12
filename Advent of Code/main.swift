4310fb915c0149fff0f7d14f50be3efe0f80298f
Dimi Chakarov <dimitar@dchakarov.com>
2023-12-12 00:09:59 +0000

    Part 1

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -4,36 +4,53 @@
 //
 
 import Foundation
-import RegexHelper
+
+struct Node {
+    let left: String
+    let right: String
+}
 
 func main() {
     let fileUrl = URL(fileURLWithPath: "./aoc-input")
     guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
     
-    let lines = inputString.components(separatedBy: "\n")
+    var lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
     
-    // Sample algorithm
-    var scoreboard = [String: Int]()
+    let ins = lines.removeFirst()
+    let instructions = Array(ins)
+    let search = /(?<node>\w{3})\s{1}=\s{1}\((?<left>\w{3}),\s{1}(?<right>\w{3})\)/
+    
+    var nodes = [String: Node]()
+    
     lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+        if let result = try? search.wholeMatch(in: line) {
+            nodes[String(result.node)] =
+            Node(
+                left: String(result.left),
+                right: String(result.right)
+            )
+        }
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
+    
+    var currentInstructionIndex = 0
+    var currentNode = "AAA"
+    var moves = 0
+    
+    while currentNode != "ZZZ" {
+        if instructions[currentInstructionIndex] == "R" {
+            currentNode = nodes[currentNode]!.right
+        } else {
+            currentNode = nodes[currentNode]!.left
         }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
+        currentInstructionIndex += 1
+        if currentInstructionIndex == instructions.count {
+            currentInstructionIndex = 0
         }
-}
-
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+        moves += 1
+    }
+    
+    print(moves)
 }
 
 main()
